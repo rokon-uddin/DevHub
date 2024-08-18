d9fdc71ec5915cd09f635a94f643c824ce57ae5c
Mohammed Rokon Uddin <shoaib.rokon@gmail.com>
2024-08-18 16:50:02 +0600

    Initial Commit

---
+++ DevHub/DevHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  DevHubApp.swift
+//  DevHub
+//
+//  Created by Mohammed Rokon Uddin on 8/18/24.
+//
+
+import SwiftUI
+
+@main
+struct DevHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
